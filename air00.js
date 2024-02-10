54134f539d328a77ab318f1d302de4968e22a41c
Baptiste Szmoniewski <b.szmoniewski@orange.fr>
2024-02-10 09:00:35 +0100

    split

--- air00.js
+++ air00.js
@@ -1,29 +1,41 @@
-const wordsArray = process.argv.slice(2);
+const wordsArray = () => {
+    return process.argv.slice(2);
+}
 
-console.log(wordsArray.join(" ")); 
+const split = (wordsArray) => {
 
-let array = [];
-for (let i = 0; i < wordsArray.length; i++) {
-    let currentWord = wordsArray[i];
-    let wordWithoutSpaces = "";
-    let wordStarted = false;
+    let array = [];
+    for (let i = 0; i < wordsArray.length; i++) {
+        let currentWord = wordsArray[i];
+        let wordWithoutSpaces = "";
+        let wordStarted = false;
 
-    for (let j = 0; j < currentWord.length; j++) {
-        if (currentWord[j] === " ") {
-            if (wordStarted) {
-                array.push(wordWithoutSpaces);
-                wordWithoutSpaces = "";
-            }
-        } else {
+        for (let j = 0; j < currentWord.length; j++) {
+            if (currentWord[j] === " ") {
+                if (wordStarted) {
+                    array.push(wordWithoutSpaces);
+                    wordWithoutSpaces = "";
+                }
+            } else {
             wordWithoutSpaces += currentWord[j];
             wordStarted = true;
+            }
+        }
+
+        if (wordWithoutSpaces !== " ") {
+            array.push(wordWithoutSpaces);
         }
     }
 
-    // Ajouter le dernier mot si le mot n'a pas d'espace à la fin
-    if (wordWithoutSpaces !== "") {
-        array.push(wordWithoutSpaces);
+    return console.log(array.join('\n'));
+}
+
+const handleError = () => {
+    if (!process.argv.slice(2).length) {
+        console.log("Erreur : veuillez écrire une chaine de caractère");
+        process.exit(1)
     }
 }
 
-console.log(array);
+handleError();
+split(wordsArray());
