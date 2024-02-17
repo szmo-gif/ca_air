53bf06f67bd0ce5a16242e6dbf791601dd771cbd
Baptiste Szmoniewski <b.szmoniewski@orange.fr>
2024-02-17 11:08:22 +0100

    chercher l'intrus

--- air03.js
+++ air03.js
@@ -1,10 +1,51 @@
-const userArgument = process.argv.slice(2);
+//parcing
+const getArgument = () => {
+    return process.argv.slice(2);
+} 
 
-const result = [];
-for (let i = 0; i < userArgument.length; i++) {
-    if (userArgument.indexOf(userArgument[i]) === i) {
-        result.push(userArgument[i]);
+//principal function
+const LookForTheIntruder = (argument) => {
+    const numberCount = [];
+
+// Compter le nombre d'occurrences de chaque nombre
+    for (let i = 0; i < argument.length; i++) {
+        const num = argument[i];
+        if (numberCount[num]) {
+            numberCount[num]++;
+        } else {
+            numberCount[num] = 1;
+        }
     }
+// Isoler les nombres qui n'ont qu'une seule occurrence
+    const numbersWithoutPair = [];
+    for (const num in numberCount) {
+        if (numberCount[num] === 1) {
+            numbersWithoutPair.push(num);
+        }
+    }
+    return numbersWithoutPair
 }
 
-console.log(result);
+//handle error 
+const isNotArguments = () => {
+    const argument = getArgument();
+    if (!argument.length) {
+        console.log("Erreur : veuillez écrire des arguments pairs et 1 impairs");
+        return true;
+    }
+    return false;
+}
+
+// apply function
+const applyFunction = () => {
+    if (isNotArguments()) {
+        return;
+    }
+    const argument = getArgument();
+    const principalFunction = LookForTheIntruder(argument);
+
+    console.log(principalFunction);
+}
+
+applyFunction();
+
