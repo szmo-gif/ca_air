dd0bf0e27e6d8c7dca33ee20f0dfcd97af4fb65a
Baptiste Szmoniewski <b.szmoniewski@orange.fr>
2024-02-20 18:00:27 +0100

    sur chacun d'entre eux

--- air05.js
+++ air05.js
@@ -1,10 +1,51 @@
-const user = process.argv.slice(2);
+//parcing
+const getArgument = () => {
+  return process.argv.slice(2);
+};
 
-let result =[];
-for (let i = 0; i < user.length - 1; i++) {
-    if (user.length - 1 !== user[i]) {
-        result.push(user[i]);
-    }
+//principal function
+const onEachOfThem = (argument) => {
+  const isLastElement = argument.length - 1;
+  const getOperator = parseInt(argument[isLastElement]);
+  let result = [];
+  for (let i = 0; i < argument.length - 1; i++) {
+    result.push(parseInt(argument[i]) + getOperator);
+    
+  }
+  return result
 }
 
-console.log(result);
+//handle error
+const isNotArguments = () => {
+  const argument = getArgument();
+  if (!argument.length) {
+    console.log("Erreur : veuillez insérer des arguments pairs et impairs");
+    return true;
+  }
+  return false;
+};
+
+/*const isNotLastElement = () => {
+  const lastElement = arguments[arguments.length - 1];
+  const operators = ["+", "-"];
+  if (!operators.includes(lastElement.charAt(0))) {
+    console.log("Erreur : veuillez insérer un opérateur (+, -) à la fin.");
+    return true;
+  }
+  return false;
+};*/
+
+//apply function
+const displayFunction = () => {
+  if (isNotArguments()) {
+    return;
+  }
+  /*if (isNotLastElement()) {
+    return;
+  }*/
+  const argument = getArgument();
+  const principalFunction = onEachOfThem(argument);
+  console.log(principalFunction.join(" "));
+};
+
+displayFunction()
