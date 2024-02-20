b7effea4dd206c021e4ca8038b770837f87b3eaa
Baptiste Szmoniewski <b.szmoniewski@orange.fr>
2024-02-20 16:55:13 +0100

    norme js

--- air04.js
+++ air04.js
@@ -1,42 +1,46 @@
 //parcing
 const getArgument = () => {
-    const getUser = process.argv.slice(2);
-    const joinUser = getUser.join(" ");
-    const splitUser = joinUser.split('')
-    return splitUser
-} 
+    return process.argv.slice(2);
+}
+
+const joinUser = (argument) => {
+  return argument.join(" ");
+}
+const splitUser = (argumentJoin) => {
+  return  argumentJoin.split('');
+}
 
 //principal function
 const onlyOne = (splitUser) => {
-    let result = "";
-    for (let i = 0; i < splitUser.length; i++) {
-        if (splitUser[i] !== splitUser[i + 1]) {
-            result += splitUser[i]
-        }
+  let result = "";
+  for (let i = 0; i < splitUser.length; i++) {
+    if (splitUser[i] !== splitUser[i + 1]) {
+      result += splitUser[i]
+      }
     }
-    return result
+  return result
 }
 
 //handle error 
 const isNotArguments = () => {
-    const argument = getArgument();
-    if (!argument.length) {
-        console.log("Erreur : veuillez écrire une chaine de caractère.");
-        return true;
-    }
-    return false;
+  const argument = getArgument();
+  if (!argument.length) {
+    console.log("Erreur : veuillez écrire une chaine de caractère.");
+    return true;
+  }
+  return false;
 }
 
 // apply function
 const applyFunction = () => {
-    if (isNotArguments()) {
-        return;
-    }
+  if (isNotArguments()) {
+    return;
+  }
 
-    const argument = getArgument()
-    const applyFunction = onlyOne(argument)
+  const argument = splitUser(joinUser(getArgument()));
+  const applyFunction = onlyOne(argument)
 
-    console.log(applyFunction);
+  console.log(applyFunction);
 };
 
 applyFunction()
