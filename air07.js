af56151f6aa6b37fc70691fb8790ab6f5750dbb2
Baptiste Szmoniewski <b.szmoniewski@orange.fr>
2024-02-23 14:45:43 +0100

    insertion d'un tableau trié

---
+++ air07.js
@@ -0,0 +1,55 @@
+// parcing
+const getArgument = () => {
+  return process.argv.slice(2, -1);
+}
+
+const getNewElement = () => {
+  return process.argv.slice(-1);
+}
+
+// principal function
+const sortedInsert = (array, newElement) => {
+  const newArray = []
+  for (let i = 0; i < array.length; i++) {
+    newArray.push(array[i])
+  }
+
+  for (let j = 0; j < newElement.length; j++) {
+    newArray.push(newElement[j])
+  }
+
+  return newArray
+}
+
+//handle error
+const isNotArguments = (argument) => {
+  if (!argument.length) {
+    return true;
+  }
+  return false;
+}
+
+const isNotNewElement = (newElement) => {
+  if (!newElement.length) {
+    return true;
+  }
+  return false;
+}
+
+//start function
+const startFunction = () => {
+  const argument = getArgument();
+  const newElement = getNewElement();
+  if (isNotArguments(argument)) {
+    return console.log("Erreur : veuillez insérer des nombres trié");
+  }
+
+  if(isNotNewElement(newElement)) {
+    return console.log("Erreur : veuillez insérer un nombre à insérer dans le premier tableau");
+  }
+
+  const startprincipalFunction = sortedInsert(argument, newElement);
+  console.log(startprincipalFunction.sort().join(" "));
+}
+
+startFunction()
