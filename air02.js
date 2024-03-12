44435e0f9c009aefb78ce321d20c389ece7ef52a
Baptiste Szmoniewski <b.szmoniewski@orange.fr>
2024-03-12 15:31:53 +0100

    rajout du concaténator

--- air02.js
+++ air02.js
@@ -1,39 +1,41 @@
 //parcing
 const getArgument = () => {
-    return process.argv.slice(2);
+  return process.argv.slice(2);
 }
 
 //principal function
 const joinArgument = (argument) => {
-    let join = "";
-    for (let i = 0; i < argument.length; i++) {
-        join += argument[i];
-        if (i < argument.length) {
-            join += " ";
-        }
+  let join = "";
+  const concatenator = argument[argument.length - 1];
+
+  for (let i = 0; i < argument.length - 1; i++) {
+    join += argument[i];
+    if (i < argument.length - 2) {
+      join += concatenator;
     }
-    return join
+  }
+  return join
 }
 
 //handle error 
 const isNotArguments = () => {
-    const argument = getArgument();
-    if (!argument.length) {
-        console.log("Erreur : veuillez écrire des arguments dont le dernier est le séparateur");
-        return true;
-    }
-    return false;
+  const argument = getArgument();
+  if (!argument.length) {
+    console.log("Erreur : veuillez écrire des arguments dont le dernier est le séparateur");
+    return true;
+  }
+  return false;
 }
 
 // apply function
 const displayJoinArgument = () => {
-    if (isNotArguments()) {
-        return;
-    }
-    const argument = getArgument();
-    const principalFunction = joinArgument(argument);
+  if (isNotArguments()) {
+    return;
+  }
+  const argument = getArgument();
+  const principalFunction = joinArgument(argument);
 
-    return console.log(principalFunction);
+  return console.log(principalFunction);
 }
 
 displayJoinArgument();
